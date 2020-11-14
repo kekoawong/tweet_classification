513200a821624c9c41ac3c7e0c7c937ecc9d058c
Kekoa Wong <kwong6@nd.edu>
2020-11-13 22:40:44 -0500

    trees

--- code/kekoa_trees.py
+++ code/kekoa_trees.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[17]:
+import pickle
+def save_object(file_name, ob):
+    with open(file_name, 'wb') as outfile:
+        pickle.dump(ob, outfile)
 
+# In[17]:
 
 import pandas as pd
 from sklearn import tree
@@ -10,11 +14,15 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import preprocessing
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
-import pickle
 import graphviz
+
+graph_file_entropy = '../visualizations/entropy_graph.png'
+graph_file_gini = '../visualizations/gini_graph.png'
+graph_file_rf = '../visualizations/random_forest_graph.png'
 
 le = preprocessing.LabelEncoder()
 
+print('Loading')
 # Get data, separate into features and labels
 with open('../data/feature_vectors.obj', 'rb') as testing_file:
     data = pickle.load(testing_file)
@@ -33,7 +41,6 @@ data_encoded = data_df.copy()
 data_encoded["Party"] = le.fit_transform(data_encoded["Party"])
 data_encoded["Retweet"] = le.fit_transform(data_encoded["is_retweet"])
 data_encoded["language"] = le.fit_transform(data_encoded["language"])
-data_encoded
 
 
 # #### Split into testing and training data
@@ -83,14 +90,14 @@ clf_entropy.fit(x_train, y_train)
 
 # In[30]:
 
-
+print('Making graph')
 dot_data_entropy = tree.export_graphviz(clf_entropy, out_file=None,
                                 feature_names=["sentiment", "language", "dot_product", "is_retweet"],
                                 class_names=["Democrat", "Republican"],
                                 filled=True, rounded=True,
                                 special_characters=True)  
 graph_entropy = graphviz.Source(dot_data_entropy)
-graph_entropy
+save_object(graph_file_entropy, graph_entropy)
 
 
 # In[27]:
@@ -123,7 +130,8 @@ dot_data_gini = tree.export_graphviz(clf_gini, out_file=None,
                                 filled=True, rounded=True,
                                 special_characters=True)  
 graph_gini = graphviz.Source(dot_data_gini)
-
+print(graph_gini)
+save_object(graph_file_gini, graph_gini)
 
 # In[29]:
 
@@ -139,11 +147,19 @@ print("Accuracy:", metrics.accuracy_score(y_test, y_pred_gini))
 
 
 # Random Forest
-clf_rf = RandomForestClassifier(max_depth=7, min_samples_split=0.1, min_samples_leaf=0.05)
+'''clf_rf = RandomForestClassifier(max_depth=7, min_samples_split=0.1, min_samples_leaf=0.05)
 clf_rf.fit(x_train, y_train.values.ravel())
 y_pred_rf = clf_rf.predict(x_test)
 print("Predicted:", y_pred_rf)
 print("Accuracy:", metrics.accuracy_score(y_test, y_pred_rf))
+
+dot_data_rf = tree.export_graphviz(clf_rf, out_file=None,
+                                feature_names=["sentiment", "language", "dot_product", "is_retweet"],
+                                class_names=["Democrat", "Republican"],
+                                filled=True, rounded=True,
+                                special_characters=True)  
+graph_rf = graphviz.Source(dot_data_rf)
+save_object(graph_file_rf, graph_rf)'''
 
 
 # In[ ]:
